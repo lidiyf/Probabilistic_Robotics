99660a36fd4c09c909f3ef6bef742596a72fe831
ywu0220 <ywu0220@gmail.com>
2023-05-02 21:43:14 -0400

    create env

---
+++ gym-examples/gym_examples/envs/grid_world.py
@@ -0,0 +1,226 @@
+import numpy as np
+import pygame
+
+import gymnasium as gym
+from gymnasium import spaces
+
+
+class GridWorldEnv(gym.Env):
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 10}
+
+    def __init__(self, render_mode=None, size=20):
+        self.size = size  # The size of the square grid
+        self.window_size = 400 #512  # The size of the PyGame window
+
+        # Observations are dictionaries with the agent's and the target's location.
+        # Each location is encoded as an element of {0, ..., `size`}^2, i.e. MultiDiscrete([size, size]).
+        self.observation_space = spaces.Dict(
+            {
+                "agent": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "target": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "pedestrian": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "pedestrian2": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+            }
+        )
+
+        # We have 4 actions, corresponding to "right", "up", "left", "down"
+        self.action_space = spaces.Discrete(4)
+
+        """
+        The following dictionary maps abstract actions from `self.action_space` to
+        the direction we will walk in if that action is taken.
+        I.e. 0 corresponds to "right", 1 to "up" etc.
+        """
+        self._action_to_direction = {
+            0: np.array([1, 0]),
+            1: np.array([0, 1]),
+            2: np.array([-1, 0]),
+            3: np.array([0, -1]),
+        }
+
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+
+        """
+        If human-rendering is used, `self.window` will be a reference
+        to the window that we draw to. `self.clock` will be a clock that is used
+        to ensure that the environment is rendered at the correct framerate in
+        human-mode. They will remain `None` until human-mode is used for the
+        first time.
+        """
+        self.window = None
+        self.clock = None
+
+    def _get_obs(self):
+        return {"agent": self._agent_location, 
+                "target": self._target_location, 
+                "pedestrian": self._pedestrian_location, 
+                "pedestrian2": self._pedestrian2_location, 
+                }
+
+    def _get_info(self):
+        return {
+            "distance": np.linalg.norm(
+                self._agent_location - self._target_location, ord=1
+            )
+        }
+
+    def reset(self, seed=None, options=None):
+        # We need the following line to seed self.np_random
+        super().reset(seed=seed)
+        self.starting_bound = np.random.randint(0, 2)
+
+
+        # Choose the agent's location uniformly at random
+        self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
+        self._agent_location[self.starting_bound] = 0
+
+        # We will sample the target's location randomly until it does not coincide with the agent's location
+        self._target_location = self._agent_location
+        '''while np.array_equal(self._target_location, self._agent_location):
+            self._target_location = self.np_random.integers(
+                0, self.size, size=2, dtype=int
+            )'''
+        self._pedestrian_location = self.np_random.integers(0, self.size, size=2, dtype=int)
+        while np.array_equal(self._pedestrian_location, self._agent_location):
+            self._pedestrian_location = self.np_random.integers(
+                0, self.size, size=2, dtype=int
+            )
+        self._pedestrian2_location = self.np_random.integers(0, self.size, size=2, dtype=int)
+        while np.array_equal(self._pedestrian2_location, self._agent_location):
+            self._pedestrian2_location = self.np_random.integers(
+                0, self.size, size=2, dtype=int
+            )
+
+        observation = self._get_obs()
+        info = self._get_info()
+
+        if self.render_mode == "human":
+            self._render_frame()
+
+        return observation, info
+
+    def step(self, action):
+        # Map the action (element of {0,1,2,3}) to the direction we walk in
+        direction = self._action_to_direction[action]
+        # We use `np.clip` to make sure we don't leave the grid
+        self._agent_location = np.clip(
+            self._agent_location + direction, 0, self.size - 1
+        )
+
+        target_direction = self._action_to_direction[self.action_space.sample()]
+        self._target_location = np.clip(
+            self._target_location + target_direction, 0, self.size -1
+        )
+
+        pedestrian_direction = self._action_to_direction[self.action_space.sample()]
+        self._pedestrian_location = np.clip(
+            self._pedestrian_location + pedestrian_direction, 0, self.size - 1
+        )
+        pedestrian2_direction = self._action_to_direction[self.action_space.sample()]
+        self._pedestrian2_location = np.clip(
+            self._pedestrian2_location + pedestrian2_direction, 0, self.size - 1
+        )
+        # An episode is done iff the agent has reached the target
+        #terminated = np.array_equal(self._agent_location, self._target_location)
+        terminated = self._target_location[self.starting_bound] >= self.size - 1
+        reward = 1 if terminated else 0  # Binary sparse rewards
+        observation = self._get_obs()
+        info = self._get_info()
+
+        if self.render_mode == "human":
+            self._render_frame()
+
+        return observation, reward, terminated, False, info
+    
+    def calc_rewards(self):
+        pass
+
+    def render(self):
+        if self.render_mode == "rgb_array":
+            return self._render_frame()
+
+    def _render_frame(self):
+        if self.window is None and self.render_mode == "human":
+            pygame.init()
+            pygame.display.init()
+            self.window = pygame.display.set_mode(
+                (self.window_size, self.window_size)
+            )
+        if self.clock is None and self.render_mode == "human":
+            self.clock = pygame.time.Clock()
+
+        canvas = pygame.Surface((self.window_size, self.window_size))
+        canvas.fill((255, 255, 255))
+        pix_square_size = (
+            self.window_size / self.size
+        )  # The size of a single grid square in pixels
+
+        # First we draw the target
+        pygame.draw.rect(
+            canvas,
+            (255, 0, 0),
+            pygame.Rect(
+                pix_square_size * self._target_location,
+                (pix_square_size, pix_square_size),
+            ),
+        )
+        # Now we draw the agent
+        pygame.draw.circle(
+            canvas,
+            (0, 0, 255),
+            (self._agent_location + 0.5) * pix_square_size,
+            pix_square_size / 3,
+        )
+
+        pygame.draw.circle(
+            canvas,
+            (0, 0, 0),
+            (self._pedestrian_location + 0.5) * pix_square_size,
+            pix_square_size / 3,
+            width=1,
+        )
+
+        pygame.draw.circle(
+            canvas,
+            (0, 0, 0),
+            (self._pedestrian2_location + 0.5) * pix_square_size,
+            pix_square_size / 3,
+            width=1,
+        )
+
+        # Finally, add some gridlines
+        '''for x in range(self.size + 1):
+            pygame.draw.line(
+                canvas,
+                0,
+                (0, pix_square_size * x),
+                (self.window_size, pix_square_size * x),
+                width=3,
+            )
+            pygame.draw.line(
+                canvas,
+                0,
+                (pix_square_size * x, 0),
+                (pix_square_size * x, self.window_size),
+                width=3,
+            )'''
+
+        if self.render_mode == "human":
+            # The following line copies our drawings from `canvas` to the visible window
+            self.window.blit(canvas, canvas.get_rect())
+            pygame.event.pump()
+            pygame.display.update()
+
+            # We need to ensure that human-rendering occurs at the predefined framerate.
+            # The following line will automatically add a delay to keep the framerate stable.
+            self.clock.tick(self.metadata["render_fps"])
+        else:  # rgb_array
+            return np.transpose(
+                np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
+            )
+    
+    def close(self):
+        if self.window is not None:
+            pygame.display.quit()
+            pygame.quit()
